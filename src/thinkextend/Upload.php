910950204ff5ce49749eaf9658e76b30d90bebc0
申法宽 <shenfakuan@prope.com.cn>
2016-05-05 14:53:49 +0800

    修改 增加三个空类

---
+++ src/thinkextend/Upload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 申法宽
+ * Date: 16/5/5
+ * Time: 14:53
+ * Email: sfk@live.cn
+ * File: Upload.php
+ */
+
+namespace shenfakuan\thinkextend;
+
+
+class Upload
+{
+
+}
---
+++ src/thinkextend/Verify.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 申法宽
+ * Date: 16/5/5
+ * Time: 14:52
+ * Email: sfk@live.cn
+ * File: Verify.php
+ */
+
+namespace shenfakuan\thinkextend;
+
+
+class Verify
+{
+
+}
