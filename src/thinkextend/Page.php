4112ec73a352e029f0b9cba5381bef44db1065e9
申法宽 <shenfakuan@prope.com.cn>
2016-05-05 13:57:21 +0800

    修改

--- src/thinkextend/Page.php
+++ src/thinkextend/Page.php
@@ -8,7 +8,7 @@
 // +----------------------------------------------------------------------
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
-namespace thinkextend;
+namespace shenfakuan\thinkextend;
 
 use think\Config;
 
